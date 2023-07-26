031c4c3dcf38c322c4a3ab7a5e068be7b89e511e
Minh Vo <Minh.Vo@Student.htw-berlin.de>
2023-07-26 10:44:58 +0200

    Finale Version
    Überblendung und color laying möglich
    Slider funktioniert
    Color Layer Buttons funktionieren
    Abspielen von 2 Videos und 2 Audios möglich
    Reset, Loop, Unloop und Pause möglich
    
    kleine UI fixes

--- index.js
+++ index.js
@@ -71,6 +71,11 @@ function handleFileSelect(evt, type, nr) {
             videoPlayer.innerHTML = '';
             videoPlayer.appendChild(video);
             videoLoadedStatus[nr] = true;
+
+
+            if (Object.keys(videoLoadedStatus).length === 2) {
+                overlayVideos();
+            }
         }
     }
     //Add audio logic
@@ -137,6 +142,59 @@ function handleFileSelect(evt, type, nr) {
         }
     })
 }
+
+/* const videoPlayer = document.getElementById('Video' + nr);
+if (videoPlayer) {
+    videoPlayer.innerHTML = '';
+    videoPlayer.appendChild(video);
+    videoLoadedStatus[nr] = true;
+    //Video control logic
+    // const playButton = document.getElementById('playButton' + nr);
+    const volumeSlider = document.getElementById('videoVolumeSlider' + nr);
+    const playbackSpeedSlider = document.getElementById('videoPlaybackSpeedSlider' + nr);
+    //const resetButton = document.getElementById('videoResetButton' + nr);
+
+    /*  playButton.addEventListener('click', () => {
+         if (video.paused) {
+             video.play();
+         } else {
+             video.pause();
+         }
+     }); 
+
+const videoResetButton1 = document.getElementById('resetButton1');
+videoResetButton1.addEventListener('click', function () {
+    resetVideoTrack(1);
+})
+
+const videoResetButton2 = document.getElementById('resetButton2');
+videoResetButton2.addEventListener('click', function () {
+    resetVideoTrack(2);
+})
+
+/*    resetButton.addEventListener('click', () => {
+       resetVideoTrack(nr);
+   });
+
+volumeSlider.addEventListener('input', () => {
+    video.volume = volumeSlider.value / 100;
+});
+
+playbackSpeedSlider.addEventListener('input', () => {
+    video.playbackRate = playbackSpeedSlider.value;
+});
+
+const loopButton = document.getElementById('loopVideoButton' + nr);
+
+loopButton.addEventListener('click', () => {
+    video.loop = !video.loop;
+    if (video.loop) {
+        loopButton.innerHTML = "Unloop";
+    } else {
+        loopButton.innerHTML = "Loop";
+    }
+});
+} */
 
 // Funktion zum Zurücksetzen des hochgeladenen Audiotracks
 function resetAudioTrack(nr) {
@@ -209,4 +267,75 @@ function visualizeAudio(audio, nr) {
         requestAnimationFrame(animate);
     }
     animate();
+}
+
+function overlayVideos() {
+    const canvas = document.getElementById('chromaVideo');
+    const ctx = canvas.getContext('2d');
+
+    const video1 = document.getElementById('uploadedVideo1');
+    const video2 = document.getElementById('uploadedVideo2');
+
+    const chromaSlider = document.getElementById('chromaSlider');
+
+    // Checkbox elements for Video1
+    const video1RedCheckbox = document.getElementById('video1RedCheckbox');
+    const video1GreenCheckbox = document.getElementById('video1GreenCheckbox');
+    const video1BlueCheckbox = document.getElementById('video1BlueCheckbox');
+
+    // Checkbox elements for Video2
+    const video2RedCheckbox = document.getElementById('video2RedCheckbox');
+    const video2GreenCheckbox = document.getElementById('video2GreenCheckbox');
+    const video2BlueCheckbox = document.getElementById('video2BlueCheckbox');
+
+    canvas.width = video1.videoWidth;
+    canvas.height = video1.videoHeight;
+
+    function drawVideos() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Filter Video1 based on the checkboxes
+        let video1Filtered = filterColor(video1, video1RedCheckbox.checked, video1GreenCheckbox.checked, video1BlueCheckbox.checked);
+
+        ctx.drawImage(video1Filtered, 0, 0, canvas.width, canvas.height);
+
+        const alpha = parseFloat(chromaSlider.value) / 100;
+        ctx.globalAlpha = alpha;
+        ctx.drawImage(video2, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1;
+
+        requestAnimationFrame(drawVideos);
+    }
+
+    // Function to apply color filtering to the video based on checkbox states
+    function filterColor(video, red, green, blue) {
+        const tempCanvas = document.createElement('canvas');
+        const tempCtx = tempCanvas.getContext('2d');
+        tempCanvas.width = video.videoWidth;
+        tempCanvas.height = video.videoHeight;
+        tempCtx.drawImage(video, 0, 0, tempCanvas.width, tempCanvas.height);
+
+        const imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length; i += 4) {
+            if (red) {
+                data[i + 1] = 0; // Set green channel to 0
+                data[i + 2] = 0; // Set blue channel to 0
+            }
+            if (green) {
+                data[i] = 0; // Set red channel to 0
+                data[i + 2] = 0; // Set blue channel to 0
+            }
+            if (blue) {
+                data[i] = 0; // Set red channel to 0
+                data[i + 1] = 0; // Set green channel to 0
+            }
+        }
+
+        tempCtx.putImageData(imageData, 0, 0);
+        return tempCanvas;
+    }
+
+    drawVideos();
 }
